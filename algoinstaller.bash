9c3301c7716be18e8e85c4c108a9a9fba548c211
Thy_Dye <122063258+thy-dye@users.noreply.github.com>
2023-02-13 21:29:08 -0800

    Create algoinstaller.bash
    Automatically updates and installs dependencies necessary for Algo VPN. Installs actual Algo VPN files and installs the rest of algos dependencies.

---
+++ algoinstaller.bash
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+#update ubuntu and install virtualenv for new users
+sudo apt update
+sudo apt install -y python3-virtualenv
+
+#adds all of algo's files
+git clone https://github.com/trailofbits/algo && 
+cd algo
+
+#installs the rest of algos dependencies
+python3 -m virtualenv --python="$(command -v python3)" .env &&
+  source .env/bin/activate &&
+  python3 -m pip install -U pip virtualenv &&
+  python3 -m pip install -r requirements.txt
+
+#information letting user know it is done
+echo
+echo  *algo installation complete*
+echo
+
+exec bash
